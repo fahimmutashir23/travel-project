1ad9e28e1f9714a70f1aa88748bbd1a6dae50ba3
Labonya <1aklimakter@gmail.com>
2023-12-13 22:07:17 +0600

    added about section and news section from aalabonya

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,20 @@
 
+import News from '../../components/News/News';
 import PopularDestination from '../../components/PopularDestination/PopularDestination';
 import Banner from '../../components/Shared/Banner';
 import Weekly from '../../components/Weekly/Weekly';
 import WhyChoose from '../../components/WhyChoose/WhyChoose';
+import About from '../About/About';
 
 const Home = () => {
     return (
         <div>
             <Banner/>
+            <About/>
             <PopularDestination/>
             <WhyChoose/>
             <Weekly/>
+         <News/>
         </div>
     );
 };
