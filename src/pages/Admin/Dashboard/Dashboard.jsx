a3e951bc4257242414c1775c918e21cb858d3869
Labonya <1aklimakter@gmail.com>
2024-01-16 14:08:13 +0600

    added chart

--- src/pages/Admin/Dashboard/Dashboard.jsx
+++ src/pages/Admin/Dashboard/Dashboard.jsx
@@ -6,24 +6,87 @@ import { MdMarkEmailUnread } from "react-icons/md";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../../../Hooks/useAxiosPublic";
 import Loader from "../../../Utils/Loader/Loader";
-  // eslint-disable-next-line no-unused-vars
+import { PieChart, Pie, Cell, Legend } from 'recharts';
+import { BarChart, Bar,XAxis, YAxis, CartesianGrid } from 'recharts';
+// eslint-disable-next-line no-unused-vars
 const Dashboard = () => {
 
 
- const axiosPublic= useAxiosPublic()
+  const axiosPublic = useAxiosPublic()
 
   const { data: statistics = [], isLoading } = useQuery({
-		queryKey: ['stats'],
-		queryFn: async () => {
-			const res = await axiosPublic.get("/stats")
-			return res.data;
-		}
+    queryKey: ['stats'],
+    queryFn: async () => {
+      const res = await axiosPublic.get("/stats")
+      return res.data;
+    }
 
-	})
-  
-if(isLoading){
-  return <Loader width='20' center='center' />
-}
+  })
+  const data = [
+    { name: 'users', value: parseInt(statistics?.users) },
+    { name: 'TotalBookings', value: parseInt(statistics?.totalBookings) },
+    { name: 'TotalPackages', value: parseInt(statistics?.totalPackages) },
+    { name: 'TotalRevenue', value: parseInt(statistics?.revenue / 1000) },
+  ];
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red'];
+  const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+  const data1 = [
+    {
+      name: 'users',
+      uv: statistics?.users,
+      pv: statistics?.totalBookings,
+      amt: statistics?.revenue / 1000,
+    },
+    {
+      name: 'Bookings',
+      uv: statistics?.totalBookings,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Packages',
+      uv: statistics?.totalPackages,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'TotalRevenue',
+      uv: statistics?.revenue / 1000,
+      pv: 3908,
+      amt: 2000,
+    }
+  ];
+
+  const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+    ${x + width / 2}, ${y}
+    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+    Z`;
+  };
+
+  const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
+
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+  };
+
+
+
+  if (isLoading) {
+    return <Loader width='20' center='center' />
+  }
 
   return (
     <div>
@@ -44,7 +107,7 @@ if(isLoading){
         </div>
 
         <div className="border flex justify-between items-center w-60 h-20 rounded-lg">
-          <div className="h-full text-center bg-pink-700 p-6"><LuPackagePlus className="text-white text-center w-16 h-9"/></div>
+          <div className="h-full text-center bg-pink-700 p-6"><LuPackagePlus className="text-white text-center w-16 h-9" /></div>
           <div className="p-5"><small>Hotel Package</small><br />{statistics.totalPackages}</div>
         </div>
         <div className="border flex justify-between items-center w-56 h-20 rounded-lg">
@@ -53,6 +116,50 @@ if(isLoading){
         </div>
       </div>
 
+      <div className="flex items-center justify-center">
+        <div className="">
+          <PieChart width={400} height={400}>
+            <Pie
+              data={data}
+              cx="50%"
+              cy="50%"
+              labelLine={false}
+              label={renderCustomizedLabel}
+              outerRadius={80}
+              fill="#8884d8"
+              dataKey="value"
+            >
+              {data.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+              ))}
+            </Pie>
+            <Legend></Legend>
+          </PieChart>
+
+        </div>
+        <div><BarChart
+          width={500}
+          height={300}
+          data={data1}
+          margin={{
+            top: 20,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        ><CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Bar dataKey="uv" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+          {data1.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+          ))}
+        </Bar>
+      </BarChart>
+
+
+        </div>
+      </div>
       {/* <div className="mt-24">
         <iframe
           className=""
