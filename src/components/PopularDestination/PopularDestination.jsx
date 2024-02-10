75c693616d4f10d58db35313ed7431db3316e34f
Labonya <1aklimakter@gmail.com>
2024-02-11 02:30:49 +0600

    added

--- src/components/PopularDestination/PopularDestination.jsx
+++ src/components/PopularDestination/PopularDestination.jsx
@@ -5,7 +5,7 @@ const PopularDestination = () => {
     <div>
       <p className="text-center text-lg -mb-5 mt-10 font-bold">Discover</p>
       <PageTitleForHome title='Popular Destination' />
-      {/* <div className="grid grid-cols-2 sm:grid-cols-4 gap-2">
+       {/* <div className="grid grid-cols-2 sm:grid-cols-4 gap-2">
         <div className="space-y-2">
           <a href="#" className="group relative block overflow-hidden">
             <button className="absolute end-4 top-4 z-10 rounded-full bg-white p-1.5 text-gray-900 transition hover:text-gray-900/75">
@@ -85,173 +85,102 @@ const PopularDestination = () => {
             alt="Image Description"
           />
         </div>
-      </div> */}
-      <section className="py-6 dark:bg-gray-800 dark:text-gray-50">
-	<div className="container grid grid-cols-2 gap-4 p-4 mx-auto md:grid-cols-4">
-		<img src="https://source.unsplash.com/random/301x301/" alt="" className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-3 md:row-start-1 dark:bg-gray-500 aspect-square" />
-		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?0" />
-		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?1" />
-		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?2" />
-		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?3" />
-		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?4" />
-		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?5" />
-		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?6" />
-		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?7" />
-		<img src="https://source.unsplash.com/random/302x302/" alt="" className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-1 md:row-start-3 dark:bg-gray-500 aspect-square" />
-	</div>
-</section>
-{/* <div className="container mx-auto space-y-12">
+      </div>  */}
+
+<div className="container mx-auto">
 		<div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row">
-   <div className="w-2/3">
+   <div className="w-[55%]">
    <div className="w-full">
-  <div className="relative right-0">
-    <ul className="relative flex flex-wrap p-1 list-none rounded-xl bg-blue-gray-50/60" data-tabs="tabs" role="list">
-      <li className="z-30 flex-auto text-center">
-        <a className="z-30 flex items-center justify-center w-full px-0 py-1 mb-0 transition-all ease-in-out border-0 rounded-lg cursor-pointer text-slate-700 bg-inherit"
-          data-tab-target="" role="tab" aria-selected="true" aria-controls="app">
-          <span className="ml-1">weekly gallery</span>
-        </a>
-      </li>
-      <li className="z-30 flex-auto text-center">
-        <a className="z-30 flex items-center justify-center w-full px-0 py-1 mb-0 transition-all ease-in-out border-0 rounded-lg cursor-pointer text-slate-700 bg-inherit"
-          data-tab-target="" role="tab" aria-selected="false" aria-controls="message">
-          <span className="ml-1">Popular destination</span>
-        </a>
-      </li>
-      <li className="z-30 flex-auto text-center">
-        <a className="z-30 flex items-center justify-center w-full px-0 py-1 mb-0 transition-all ease-in-out border-0 rounded-lg cursor-pointer text-slate-700 bg-inherit"
-          data-tab-target="" role="tab" aria-selected="false" aria-controls="settings">
-          <span className="ml-1">Packages</span>
-        </a>
-      </li>
-    </ul>
-    <div data-tab-content="" className="p-5">
-      <div className="block opacity-100" id="app" role="tabpanel">
-        <div role="tabpanel"
-          className="grid w-full grid-cols-3 gap-4 p-4 font-sans text-base antialiased font-light leading-relaxed text-gray-700 h-max"
-          data-value="html">
-          <div>
-            <img className="w-full h-40 max-w-full rounded-lg"
-              src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2940&amp;q=80"
-              alt="image-photo" />
-          </div>
-          <div>
-            <img className="w-full h-40 max-w-full rounded-lg"
-              src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2762&amp;q=80"
-              alt="image-photo" />
-          </div>
-          <div>
-            <img className="w-full h-40 max-w-full rounded-lg"
-              src="https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2832&amp;q=80"
-              alt="image-photo" />
-          </div>
-          <div>
-            <img className="w-full h-40 max-w-full rounded-lg"
-              src="https://demos.creative-tim.com/material-kit-pro/assets/img/examples/blog5.jpg" alt="image-photo" />
-          </div>
-          <div>
-            <img className="w-full h-40 max-w-full rounded-lg"
-              src="https://material-taillwind-pro-ct-tailwind-team.vercel.app/img/content2.jpg" alt="image-photo" />
-          </div>
-          <div>
-            <img className="w-full h-40 max-w-full rounded-lg"
-              src="https://images.unsplash.com/photo-1620064916958-605375619af8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1493&amp;q=80"
-              alt="image-photo" />
-          </div>
-          <div>
-            <img className="w-full h-40 max-w-full rounded-lg"
-              src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2940&amp;q=80"
-              alt="image-photo" />
-          </div>
-          <div>
-            <img className="w-full h-40 max-w-full rounded-lg"
-              src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2762&amp;q=80"
-              alt="image-photo" />
-          </div>
-          <div>
-            <img className="w-full h-40 max-w-full rounded-lg"
-              src="https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2832&amp;q=80"
-              alt="image-photo" />
-          </div>
-         
-         
-         
-        </div>
-      </div>
-      <div className="hidden opacity-0" id="message" role="tabpanel">
-        <div role="tabpanel"
-          className="grid w-full grid-cols-3 gap-4 p-4 font-sans text-base antialiased font-light leading-relaxed text-gray-700 h-max"
-          data-value="html">
-          <div>
-            <img className="w-full h-40 max-w-full rounded-lg"
-              src="https://demos.creative-tim.com/material-kit-pro/assets/img/examples/blog5.jpg" alt="image-photo" />
-          </div>
-          <div>
-            <img className="w-full h-40 max-w-full rounded-lg"
-              src="https://material-taillwind-pro-ct-tailwind-team.vercel.app/img/content2.jpg" alt="image-photo" />
-          </div>
-          <div>
-            <img className="w-full h-40 max-w-full rounded-lg"
-              src="https://images.unsplash.com/photo-1620064916958-605375619af8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1493&amp;q=80"
-              alt="image-photo" />
-          </div>
-          <div>
-            <img className="w-full h-40 max-w-full rounded-lg"
-              src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2940&amp;q=80"
-              alt="image-photo" />
-          </div>
-          <div>
-            <img className="w-full h-40 max-w-full rounded-lg"
-              src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2762&amp;q=80"
-              alt="image-photo" />
-          </div>
-          <div>
-            <img className="w-full h-40 max-w-full rounded-lg"
-              src="https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2832&amp;q=80"
-              alt="image-photo" />
-          </div>
-        </div>
-      </div>
-      <div className="hidden opacity-0" id="settings" role="tabpanel">
-        <div role="tabpanel"
-          className="grid w-full grid-cols-3 gap-4 p-4 font-sans text-base antialiased font-light leading-relaxed text-gray-700 h-max"
-          data-value="html">
-          <div>
-            <img className="w-full h-40 max-w-full rounded-lg"
-              src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2940&amp;q=80"
-              alt="image-photo" />
-          </div>
-          <div>
-            <img className="w-full h-40 max-w-full rounded-lg"
-              src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2762&amp;q=80"
-              alt="image-photo" />
-          </div>
-          <div>
-            <img className="w-full h-40 max-w-full rounded-lg"
-              src="https://demos.creative-tim.com/material-kit-pro/assets/img/examples/blog5.jpg" alt="image-photo" />
-          </div>
-          <div>
-            <img className="w-full h-40 max-w-full rounded-lg"
-              src="https://material-taillwind-pro-ct-tailwind-team.vercel.app/img/content2.jpg" alt="image-photo" />
-          </div>
-          <div>
-            <img className="w-full h-40 max-w-full rounded-lg"
-              src="https://images.unsplash.com/photo-1620064916958-605375619af8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1493&amp;q=80"
-              alt="image-photo" />
-          </div>
- 
-          <div>
-            <img className="w-full h-40 max-w-full rounded-lg"
-              src="https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2832&amp;q=80"
-              alt="image-photo" />
-          </div>
-        </div>
-      </div>
+   <div className="grid grid-cols-2 gap-4 md:grid-cols-3">
+  <div className="grid gap-4">
+    <div>
+      <img
+        className="h-auto max-w-full rounded-lg object-cover object-center"
+        src="https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1950&amp;q=80"
+        alt="gallery-photo"
+      />
+    </div>
+    <div>
+      <img
+        className="h-auto max-w-full rounded-lg object-cover object-center "
+        src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=927&amp;q=80"
+        alt="gallery-photo"
+      />
+    </div>
+    <div>
+      <img
+        className="h-auto max-w-full rounded-lg object-cover object-center"
+        src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2940&amp;q=80"
+        alt="gallery-photo"
+      />
     </div>
   </div>
+  <div className="grid gap-4">
+    <div>
+      <img
+        className="h-auto max-w-full rounded-lg object-cover object-center"
+        src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80"
+        alt="gallery-photo"
+      />
+    </div>
+    <div>
+      <img
+        className="h-auto max-w-full rounded-lg object-cover object-center"
+        src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+        alt="gallery-photo"
+      />
+    </div>
+    <div>
+      <img
+        className="h-auto max-w-full rounded-lg object-cover object-center "
+        src="https://docs.material-tailwind.com/img/team-3.jpg"
+        alt="gallery-photo"
+      />
+    </div>
+  </div>
+  <div className="grid gap-4">
+    <div>
+      <img
+        className="h-auto max-w-full rounded-lg object-cover object-center"
+        src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2940&amp;q=80"
+        alt="gallery-photo"
+      />
+    </div>
+    <div>
+      <img
+        className="h-auto max-w-full rounded-lg object-cover object-center "
+        src="https://docs.material-tailwind.com/img/team-3.jpg"
+        alt="gallery-photo"
+      />
+    </div>
+    <div>
+      <img
+        className="h-auto max-w-full rounded-lg object-cover object-center"
+        src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80"
+        alt="gallery-photo"
+      />
+    </div>
+  </div>
+  {/* <div className="grid gap-4">
+    <div>
+      <img
+        className="h-auto max-w-full rounded-lg object-cover object-center"
+        src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80"
+        alt="gallery-photo"
+      />
+    </div>
+    <div>
+      <img
+        className="h-auto max-w-full rounded-lg object-cover object-center"
+        src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=927&amp;q=80"
+        alt="gallery-photo"
+      />
+    </div>
+  </div> */}
+</div>
 </div>
    </div>
-			<div className="flex flex-col justify-center flex-1 p-6 dark:bg-gray-900">
+			<div className="flex flex-col justify-center flex-1 -mt-[580px] p-6 dark:bg-gray-900">
       <h3 className="text-3xl font-bold">KNOW ABOUT US</h3>
 				<p className="my-6 dark:text-gray-400">Welcome to our world of wanderlust! Embark on unforgettable
                 journeys, discover hidden gems, and create timeless memories
@@ -266,7 +195,7 @@ const PopularDestination = () => {
                 guide you to the extraordinary..</p>
 			</div>
 		</div>
-    </div> */}
+    </div>
     </div>
   );
 };
