f609fea15633f6852accb3051bdb6d86d72c627e
Labonya <1aklimakter@gmail.com>
2024-02-11 03:05:20 +0600

    added

--- src/components/PopularDestination/PopularDestination.jsx
+++ src/components/PopularDestination/PopularDestination.jsx
@@ -94,18 +94,41 @@ const PopularDestination = () => {
    <div className="grid grid-cols-2 gap-4 md:grid-cols-3">
   <div className="grid gap-4">
     <div>
-      <img
-        className="h-auto max-w-full rounded-lg object-cover object-center"
-        src="https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1950&amp;q=80"
+      {/* <img
+        className="h-44 w-96 rounded-lg object-cover object-center"
+        src="https://i.postimg.cc/wx2RDsjw/pexels-ahmet-polat-9289144.jpg"
         alt="gallery-photo"
-      />
+      /> */}
     </div>
     <div>
-      <img
-        className="h-auto max-w-full rounded-lg object-cover object-center "
-        src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=927&amp;q=80"
+      {/* <img
+        className=" max-w-full rounded-lg object-cover object-center h-[480px]"
+        src="https://i.postimg.cc/BZpFMGZV/pexels-confinedriley-11595339.jpg"
         alt="gallery-photo"
-      />
+      /> */}
+        <a href="#" className="group relative block bg-black max-w-full h-[480px] rounded-xl">
+  <img
+    alt=""
+    src="https://i.postimg.cc/BZpFMGZV/pexels-confinedriley-11595339.jpg"
+    className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50 rounded-xl"
+  />
+
+  <div className="relative p-4 sm:p-6 lg:p-8">
+    <p className="text-sm font-medium uppercase tracking-widest text-pink-500">new zealand</p>
+
+    <p className="text-xl font-bold text-white sm:text-2xl">new zealand</p>
+
+    <div className="mt-32 sm:mt-48 lg:mt-72">
+      <div
+        className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
+      >
+        <p className="text-sm text-white">
+        A group of passionate adventurers dedicated to creating the best journey for every traveler
+        </p>
+      </div>
+    </div>
+  </div>
+</a>
     </div>
     <div>
       <img
@@ -154,17 +177,40 @@ const PopularDestination = () => {
       />
     </div>
     <div>
-      <img
+      {/* <img
         className="h-auto max-w-full rounded-lg object-cover object-center"
         src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80"
         alt="gallery-photo"
-      />
+      /> */}
+      <a href="#" className="group relative block bg-black max-w-full h-[400px] rounded-xl">
+  <img
+    alt=""
+    src="https://i.postimg.cc/5N9BV2kJ/pexels-yusuke-furuya-9477630.jpg"
+    className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50 rounded-xl"
+  />
+
+  <div className="relative p-4 sm:p-6 lg:p-8">
+    <p className="text-sm font-medium uppercase tracking-widest text-pink-500">new zealand</p>
+
+    <p className="text-xl font-bold text-white sm:text-2xl">new zealand</p>
+
+    <div className="mt-32 sm:mt-48 lg:mt-44">
+      <div
+        className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
+      >
+        <p className="text-sm text-white">
+        A group of passionate adventurers dedicated to creating the best journey for every traveler
+        </p>
+      </div>
+    </div>
+  </div>
+</a>
     </div>
   </div>
 </div>
 </div>
    </div>
-			<div className="flex flex-col justify-center flex-1 -mt-[580px] p-6 dark:bg-gray-900">
+			<div className="flex flex-col justify-center flex-1 -mt-[280px] p-6 dark:bg-gray-900">
       <h3 className="text-3xl font-bold">KNOW ABOUT US</h3>
 				<p className="my-6 dark:text-gray-400">Welcome to our world of wanderlust! Embark on unforgettable
                 journeys, discover hidden gems, and create timeless memories
@@ -177,6 +223,9 @@ const PopularDestination = () => {
                 itineraries ensure an enchanting travel experience every step of
                 the way. Start planning your next adventure today and let us
                 guide you to the extraordinary..</p>
+
+                <p className="mt-10">Wonderful trip to the Sundarbans with the professionals at Taabu Tours. Organization was like a clockwork, can absolutely recommend these guys. Jafar, the CEO of Taabu Tours is a really nice person who is so knowledgeable, and willing to share vast source of information. On my next trip to Bangladesh..</p>
+                <p className="mt-10">Wonderful trip to the Sundarbans with the professionals at Taabu Tours. Organization was like a clockwork, can absolutely recommend these guys. Jafar, the CEO of Taabu Tours is a really nice person who is so knowledgeable, and willing to share vast source of information. On my next trip to Bangladesh..</p>
 			</div>
 		</div>
     </div>
