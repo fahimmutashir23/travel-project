36501aa70a6d6902052707a995245484f0d229ef
Labonya <1aklimakter@gmail.com>
2024-02-11 02:04:24 +0600

    added

--- src/components/Shared/Deals.jsx
+++ src/components/Shared/Deals.jsx
@@ -14,23 +14,17 @@ export const Deals = () => {
 			<div className="relative text-start px-6">
 				
 				<p className=" py-7 font-bold text-5xl mt-12 text-white">Discover Special Deals!</p>
-				<p className=" text-white">Make sure to check out these special promotions</p>
+				<p className=" text-white leading-[150%] font-semibold">Make sure to check out these special <br/> promotions</p>
 				<button className="text-white mt-14 bg-blue-500 px-4 py-2 rounded-lg font-semibold ">SEE TOUR </button>
 			</div>
 			
 			
 		</div>
-		<div className="flex flex-col items-center max-w-lg mx-12 lg:mx-0">
+		<div className="flex flex-col items-center max-w-lg mx-12 lg:mx-0 bg-slate-200 rounded-xl">
 			<div className="relative text-center">
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="absolute top-0 left-0 w-8 h-8 dark:text-gray-700">
-					<path fill="currentColor" d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
-					<path fill="currentColor" d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
-				</svg>
-				<p className="px-6 py-1 text-lg italic">Accusantium illum cupiditate harum asperiores iusto quos quasi quis quae! Fugit doloribus, voluptatum quidem magnam velit excepturi nobis, reprehenderit ducimus incidunt quisquam quae veritatis, quos iure harum.</p>
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="absolute bottom-0 right-0 w-8 h-8 dark:text-gray-700">
-					<path fill="currentColor" d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
-					<path fill="currentColor" d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
-				</svg>
+				
+				<p className="py-7 font-bold text-5xl mt-12 text-black">Don’t miss a thing</p>
+				
 			</div>
 			<span className="w-12 h-1 my-2 rounded-lg dark:bg-violet-400"></span>
 			<p>Leroy Jenkins</p>
