e4c4d717ae8cad4c9785184fa50df6be6ca4b5b6
Labonya <1aklimakter@gmail.com>
2024-02-12 22:39:28 +0600

    added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -10,7 +10,7 @@ export const Blog = () => {
                     <a rel="noopener noreferrer" href="#" className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-900">
                         <img src="https://i.postimg.cc/QMtCnQtT/pexels-josh-hild-2662183.jpg" alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500" />
                         <div className="p-6 space-y-2 lg:col-span-5">
-                            <h3 className="text-xl font-semibold sm:text-3xl group-hover:underline group-focus:underline">Noster tincidunt reprimique ad pro</h3>
+                            <h3 className="text-xl font-semibold sm:text-3xl group-hover:underline group-focus:underline">Creating The Best Journey For Every Traveler</h3>
                             <span className="text-xs dark:text-gray-400">February 19, 2021</span>
                             <p className='text-[#6A6A6A]'>Bali Indonesia is travel heaven for luxury seekers and budget backpackers alike. There's something for everyone here.
 
