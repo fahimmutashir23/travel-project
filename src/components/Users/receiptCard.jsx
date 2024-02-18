7b530e8e74b0ac5a174d6ee32ea177e772262f52
Labonya <1aklimakter@gmail.com>
2024-02-18 15:04:00 +0600

    added

--- src/components/Users/receiptCard.jsx
+++ src/components/Users/receiptCard.jsx
@@ -1,10 +1,4 @@
 /* eslint-disable react/prop-types */
-import { VisibilityOutlined } from '@mui/icons-material';
-import { Button, Divider, IconButton, InputBase, Paper } from '@mui/material';
-import { FaEdit, FaPlusSquare } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-
-
 const ReceiptCard = ({booking}) => {
    
   console.log(booking)
@@ -32,55 +26,6 @@ const ReceiptCard = ({booking}) => {
         </td>
        
       </tr>
-        {/* <div>
-          {bookings.map((booking) => (
-            <section
-              key={booking._id}
-              className="flex gap-2 max-w-5xl mx-auto border-2 bg-base-200 px-3 py-5 rounded-md mb-5"
-            >
-              <div className="w-1/3 text-center">
-                <h1 className="text-2xl font-serif text-green-700">
-                  {booking.hotelName}
-                </h1>
-                <h1 className="text-xl font-semibold">{booking.roomName}</h1>
-                <h1 className="text-md font-semibold">{booking.userName}</h1>
-                <h1>{booking.email}</h1>
-              </div>
-              <div className="flex-1">
-                <div className="flex justify-between">
-                  <h1>
-                    Pay :{" "}
-                    <span className="font-bold text-xl">
-                      {booking.roomPrice}$
-                    </span>
-                  </h1>
-                  <div className="text-right">
-                    <h1>
-                      Transaction ID :{" "}
-                      <span className="font-semibold text-lg">
-                        {booking.transactionID}
-                      </span>
-                    </h1>
-                    <h1>
-                      Booking ID :{" "}
-                      <span className="font-semibold text-lg">
-                        our_travel{booking._id.slice(-6)}
-                      </span>
-                    </h1>
-                  </div>
-                </div>
-                <h1>Payment Date : {booking.date}</h1>
-                <h1>Check In Date : {booking.checkIn}</h1>
-                <div className="flex justify-between">
-                  <h1>Check Out Date : {booking.checkOut}</h1>
-                  <Button sx={{ backgroundColor: "green" }} variant="contained">
-                    Receipt
-                  </Button>
-                </div>
-              </div>
-            </section>
-          ))}
-        </div> */}
       </>
     );
   };
